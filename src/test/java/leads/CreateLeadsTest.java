160e312dbc37ac0eac54483565a99703d2d6d8ff
my pc <my pc@DESKTOP-J5DJM6I>
2025-12-26 15:12:03 +0530

    leads created

---
+++ src/test/java/leads/CreateLeadsTest.java
@@ -0,0 +1,24 @@
+package leads;
+
+import java.time.Duration;
+import java.util.Random;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+
+
+public class CreateLeadsTest {
+	@Test
+	public void method1()
+	{
+	System.out.println("---leads---");
+	}
+}
